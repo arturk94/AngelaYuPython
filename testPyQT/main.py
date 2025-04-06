c82bceb7f9011aff68308661b6b5c2dc164c1906
Artur KNOTHE <artur@knothe.org>
2025-04-06 09:52:01 +0200

    Adding testPyQT

---
+++ testPyQT/main.py
@@ -0,0 +1,13 @@
+import sys
+from time import strftime, gmtime
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+
+curr_time = strftime("%H:%M:%S", gmtime())
+
+app = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
+engine.quit.connect(app.quit)
+engine.load('./UI/main.qml')
+engine.rootObjects()[0].setProperty('currTime', curr_time)
+sys.exit(app.exec())
